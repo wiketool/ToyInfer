145a67f2a6f5befb9bd0ad70c6af2d07803f2b7f
wiketool <64351271+wiketool@users.noreply.github.com>
2026-03-12 23:50:00 +0800

    fix(engine): initialize sampler in Engine constructor and refactor chat method for clarity

--- src/engine.cpp
+++ src/engine.cpp
@@ -13,9 +13,12 @@ Engine::Engine(const Options& options)
       llm_config(options),
       tokenizer(options, llm_config),
       transformer(options, llm_config),
-      logits_h(std::make_unique<float[]>(llm_config.vocab_size)) {};
+      logits_h(std::make_unique<float[]>(llm_config.vocab_size)),
+      sampler(llm_config) {};
 
 void Engine::chat() {
+    uint32_t pos = 0;
+    uint32_t next_token_id;
     while (1) {
         char* line = linenoise("ToyInfer> ");
         if (line == nullptr) {
@@ -30,11 +33,24 @@ void Engine::chat() {
         tokenizer.render_prompt(prompt, line, nullptr);
         std::cout << prompt.get() << std::endl;
         tokenizer.encode(prompt.get(), token_ids, token_cnt);
+
         for (uint32_t i = 0; i < token_cnt; i++) {
             std::cout << token_ids[i] << " ";
         }
         std::cout << std::endl;
-        
+        bool assistance_end = false;
+        while (assistance_end == false && pos < options.max_seq_len) {
+            transformer.forward(token_ids[pos], pos, logits_h);
+            pos++;
+            next_token_id = sampler.sample(logits_h);
+            printf("next token id: %d\n",next_token_id);
+            if (next_token_id == llm_config.eos_token_id) {
+                assistance_end = true;
+            }
+            // printf("%s", tokenizer.decode(next_token_id));
+        }
+        printf("\n");
+
         linenoiseFree(line);
     }
 }
