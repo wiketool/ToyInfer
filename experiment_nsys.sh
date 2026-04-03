96ad60f96a51fbf80191246b9997103029c7109e
wiketool <64351271+wiketool@users.noreply.github.com>
2026-04-03 05:20:04 +0000

    prefile doing

--- experiment_nsys.sh
+++ experiment_nsys.sh
@@ -9,12 +9,29 @@ CMD="./build/ToyInfer --model_dir=models/Qwen3-4B \
 
 echo "" | tee -a $LOG
 echo "===== PD两阶段，多流，关闭CUDA Graph，bench=long | NSYS+NVTX =====" | tee -a $LOG
-echo "nsys profile --trace=cuda,nvtx,osrt --sample=none --cpuctxsw=none \
-  --capture-range=nvtx --capture-range-end=stop \
+echo "nsys profile --trace=cuda,nvtx,osrt --sample=none --cpuctxsw=none -e NSYS_NVTX_PROFILER_REGISTER_ONLY=0  \
+  --capture-range=nvtx --capture-range-end=stop -p engine.chat.turn \
   --output=nsys_pd1_st1_cg0_long_nvtx \$CMD" | tee -a $LOG
 
-nsys profile --trace=cuda,nvtx,osrt --sample=none --cpuctxsw=none \
-  --capture-range=nvtx --capture-range-end=stop \
+nsys profile \
+  --trace=cuda,nvtx,osrt \
+  --sample=none \
+  --cpuctxsw=none \
+  -e NSYS_NVTX_PROFILER_REGISTER_ONLY=0 \
+  --capture-range=nvtx \
+  --capture-range-end=stop \
+  -p engine.chat.turn \
+  --output=nsys_pd1_st1_cg0_long_nvtx \
+  ./build/ToyInfer --model_dir=models/Qwen3-4B \
+  --max_seq_len=40960 --detail_time=1 \
+  --use_dedicated_prefill=1 \
+  --use_multi_stream=1 \
+  --enable_cuda_graph=0 \
+  --bench=long
+
+
+nsys profile --trace=cuda,nvtx,osrt --sample=none --cpuctxsw=none -e NSYS_NVTX_PROFILER_REGISTER_ONLY=0 \
+  --capture-range=nvtx --capture-range-end=stop -p engine.chat.turn \
   --output=nsys_pd1_st1_cg0_long_nvtx $CMD 2>&1 | tee -a $LOG
 
 echo "" | tee -a $LOG
